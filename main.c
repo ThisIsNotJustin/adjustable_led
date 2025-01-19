b273f3a560c7f84a73c4d10b8ae5752d5017787a
ThisIsNotJustin <justincolemanonetwo@gmail.com>
2025-01-19 00:42:33 -0600

    on and off

--- main.c
+++ main.c
@@ -1,7 +1,24 @@
 #include <pigpio.h>
+#include <stdio.h>
+
+#define POT_PIN 26
+#define LED_PIN 16
 
 int main() {
+    if (gpioInitialise() < 0) {
+        fprintf(stderr, "Failed to initialize pigpio\n");
+        return 1;
+    }
 
+    gpioSetMode(POT_PIN, PI_INPUT);
+    gpioSetMode(LED_PIN, PI_OUTPUT);
 
+    while (1) {
+        int values = gpioRead(POT_PIN);
+        gpioPWM(LED_PIN, values*255);
+        usleep(10000);
+    }
+
+    gpioTerminate();
     return 0;
 }
